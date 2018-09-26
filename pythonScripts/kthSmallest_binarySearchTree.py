daa4eba9ea4129cf485f38dac7edf7afc6442dd7
Chuck Larrieu Casias <chuckinator0@yahoo.com>
2018-09-26 15:05:45 -0700

    really broke apart a binary search tree problem

--- pythonScripts/kthSmallest_binarySearchTree.py
+++ pythonScripts/kthSmallest_binarySearchTree.py
@@ -34,10 +34,66 @@ How would you optimize the kthSmallest routine?
 ## smallest element. But let's try to use the properties of BST's!
 
 
-# We can keep in mind that in a Binary Search Tree, each node is greater than its
-# left child and less than its right child.
-def kthSmallest(root,k):
-  pass
+# We can keep in mind that in a Binary Search Tree, each node's value is
+# greater than the values of each node in its left subtree and less than
+# each 
+# left child and less than its right child. I took this code from a leetcode
+# solution. My goal is to annotate and document it to understand what each part
+# is doing.
+
+class TreeNode:
+  def __init__(self,x):
+    self.val = x
+    self.right = None
+    self.left = None
+
+def kthSmallest(root, k):
+    stack = []
+    # This should work just as well with 'while True'. I'm not sure why we need the 
+    # (root or stack) statement.
+    while True:
+        # Traverse down the left side of the tree as much
+        # as you can until you reach a None (going off the tree)
+        while root:
+            stack.append(root)
+            root = root.left
+        # Go back up to the leaf
+        root = stack.pop()
+        # We have now found the next smallest (bottom leftmost) element, so we decriment k.
+        k -= 1
+        # If we have decrimented k times, we have found the kth smallest element
+        if k == 0:
+            return root.val
+        # We have gone as far left as we can, so we will now go right one step and then
+        # continue going left from there back in the while loop.
+        root = root.right
+
+# Make a binary search tree
+'''
+    4
+   / \
+  2   5
+ / \   \
+1   3   6
+'''
+node_dict = {}
+for i in range(1,7):
+    node_dict[i] = TreeNode(i)
+
+node_dict[4].left = node_dict[2]
+node_dict[4].right = node_dict[5]
+node_dict[2].left = node_dict[1]
+node_dict[2].right = node_dict[3]
+node_dict[5].left = None
+node_dict[5].right = node_dict[6]
+node_dict[1].left = None
+node_dict[1].right = None
+node_dict[3].left = None
+node_dict[3].right = None
+node_dict[6].left = None
+node_dict[6].right = None
+
+print(kthSmallest(node_dict[4],3))
 
 
 
