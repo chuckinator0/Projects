000a0641e6df2a7924ee9d42a8c0f5c3a5bda889
Chuck Larrieu Casias <chuckinator0@yahoo.com>
2018-09-26 11:46:30 -0700

    want to try BST problem without using quickselect

--- pythonScripts/kthSmallest_binarySearchTree.py
+++ pythonScripts/kthSmallest_binarySearchTree.py
@@ -25,31 +25,33 @@ Input: root = [5,3,6,2,4,null,null,1], k = 3
  1
 Output: 3
 Follow up:
-What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently?
+How would you optimize the kthSmallest routine?
 '''
 
-## For fun, check to see if we can just use quick select here (see quickselect.py)
-# from quickselect import quickselect
-
-# def kthSmallest(root,k):
-#   for element in root:
-#     if element is None:
-#       root.remove(element)
-#   return quickselect(root,k)
+## If the binary tree were given in array form, we could remove the None
+## values and use quickselect (see quickselect.py) to find the kth
+## smallest element. But let's try to use the properties of BST's!
 
 
-# Yay, it works! But it's only valid if we are given the binary tree in array form.
+# We can keep in mind that in a Binary Search Tree, each node is greater than its
+# left child and less than its right child.
+def kthSmallest(root,k):
+  pass
 
 
-# Let's try using treeNode objects
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
 
-root = [3,1,4,None,2]
-k=1
 
-print(kthSmallest(root,k))
+
+
+
+
+
+
+
+
+
+
+
+
+
