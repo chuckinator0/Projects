350b0dc2228f047a2556b6af8d662616337b2ac2
Chuck Larrieu Casias <chuckinator0@yahoo.com>
2018-10-09 17:05:19 -0700

    yayyy defeated robber problem with dynamic programming

--- scripts/apacheLogFileAnalysis.py
+++ scripts/apacheLogFileAnalysis.py
@@ -11,14 +11,20 @@ The task is to write a scrip that will count how many requests came from 127.0.0
 file = './apacheLogFileAnalysis.log'
 
 with open(file,'r') as log:
+	# keep a count of the localhost requests (requests originating from the local machine)
 	count_localhost_requests = 0
+	# keep a count of other requests
 	count_outside_requests = 0
 	for line in log:
+		# split the line into a list so that we can extract the IP address
 		line = line.split(' ')
+		# if we have a request from localhost, increase its counter
 		if line[0] == '127.0.0.1':
 			count_localhost_requests += 1
+		# there is a line with a newline character so I want to exclude that
 		elif line[0] == '\n':
 			continue
+		# if we have a request that's not localhost, increase that counter
 		else:
 			count_outside_requests += 1
 	print('There are {} localhost requests and {} outside requests' .format(count_localhost_requests,count_outside_requests))
--- scripts/houseRobber.py
+++ scripts/houseRobber.py
@@ -19,14 +19,66 @@ Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (m
              Total amount you can rob = 2 + 9 + 1 = 12.
 '''
 
-# Brute force: find all the combinations of houses where no two houses are adjacent, and take the combination
-# that gives the maximum amount of money.
+def rob(houses):
+	'''
+	This function outputs the maximum amount of money we can get from robbing a list
+	of houses whose values are the money. To do this, we will find the max amount of money
+	to rob sublists houses[0:1], houses[0:2], ..., houses[0:k+1],..., houses[0:len(houses)]
 
-def rob(house_list, maximum = [0]):
-	number_houses = len(house_list)
-	for index in number_houses:
-		house_value = house_list[index]
-		this_max = house_value + rob(house_list[ : index-2 : index+2 : ])
-		if this_max > maximum[0]:
-			maximum[0] = this_max
+	Let f(k) be the maximum money for robbing houses[0:k+1]. In other words,
+	f(k) := rob(houses[0:k]). Notice that we have this relationship:
+	f(k) == max( houses[k] + f(k-2), f(k-1) )
+	This relationship holds because the maximum money for robbing
+	houses 0 through k is either the maximum money for robbing houses
+	0 through k-2 plus robbing house k (remember, the houses can't be adjacent),
+	or, if house k isn't that much money and house k-1 is, we might get the maximum money from robbing
+	houses 0 through k-1 (which puts house k off limits due to the no-adjacency rule).
+
+	Notice that to compute f(k), we only need the values f(k-2) and f(k-1), much like fibonacci.
+	So our memo will consist of two variables that keep track of these values and update as k goes from 
+	0 to len(houses)-1.
+	'''
+	# Let's handle the case where the house list is empty
+	if not houses:
+		return 0
+	# Let's handle the cases where the houses list is only one house
+	if len(houses) == 1:
+		return houses[0]
+	# Let's handle the cases where the houses list is only two houses
+	if len(houses) == 2:
+		return max(houses[0],houses[1])
+
+
+	# initialize f(k-2) to f(0), where our sublist of houses is just the value of the first house.
+	fk_minus2 = houses[0]
+
+
+	# initialize f(k-1) to f(1), which is the max money for houses[0:3], the first two houses.
+	# We just take the max of these two house values.
+	fk_minus1 = max(houses[0], houses[1])
+
+
+
+	# now we march through the list:houses from position 2 onward, updating f(k-2), f(k-1)
+	# along the way
+	for house in houses[2:]:
+		# The max value we can get robbing houses up to and including this current house is 
+		# either this house plus the max value up to 2 houses ago, or the max value up to the last house
+		fk = max( house + fk_minus2, fk_minus1)
+		# increment k
+		fk_minus2, fk_minus1 = fk_minus1, fk
+
+	# At this point, k has reached the end of the list, and so fk represents the maximum money from robbing
+	# the entire list of houses, which is the return value of our rob function.
+	return fk
+
+houses1 = [1,2,4,2,8] # expect rob(houses1) == 13
+houses2 = [] # expect rob(houses2) is None
+
+print(rob(houses1))
+
+
+
+
+
 
