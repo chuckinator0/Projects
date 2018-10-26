6755cc6170500c59b703819248d09d6b2de2f3b6
Chuck Larrieu Casias <chuckinator0@yahoo.com>
2018-10-26 12:27:57 -0700

    improve documentation

--- scripts/log_practice.py
+++ scripts/log_practice.py
@@ -1,5 +1,11 @@
 """
 Parse a log file! See log_practice.log.
+The log is a schedule of classes on different days.
+
+Each line has the form:
+HH:MM Topic
+
+Days are separated by blank lines.
 """
 
 
@@ -18,26 +24,32 @@ def duration(string1, string2):
     return difference
 
 
-topic_dict = {}
+topic_dict = {} # {topic: duration of that topic}
 total_duration = 0
 last_event = []
 
 with open("./log_practice.log", "r") as log_file:
     for line in log_file:
+        # if we reach a blank line, it means we start a new day
         if line == "\n":
             print('\n')
             last_event = []
             continue
+        # Turn the line into an event where index 0 is the stat time and
+        # index 1 is the topic
         event = line.rstrip('\n').split(" ", 1)
         start = event[0]
         topic = event[1]
+        # if this is the first event of the day, set the last_event variable
         if last_event == []:
             last_event = event
             continue
         dur = duration(last_event[0], start)
+        # increase total duration
         total_duration += dur
         output = last_event[0] + '-' + start + " " + last_event[1]
         print(output)
+        # update the topic dictionary
         if last_event[1] in topic_dict:
             topic_dict[last_event[1]] += dur
         else:
