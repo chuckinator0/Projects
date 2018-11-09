dc0cd27dc093a5ebce4367a8b7ff82ed887186d1
Chuck Larrieu Casias <chuckinator0@yahoo.com>
2018-11-09 10:23:54 -0800

    slight modification for edge case

--- scripts/fib_play.py
+++ scripts/fib_play.py
@@ -1,21 +1,20 @@
 def fib(n):
 	'''
-	print nth fibonacci number
+	print nth fibonacci number, n > 1
 	'''
-	if n in [0,1]:
-		return n
+	if n ==1:
+		return 1
 
 	# The n-2 fibonacci number
-	two_back = 1
+	two_back = 0
 	# The n-1 fibonacci number
 	one_back = 1
 
-	for _ in range(n):
+	for _ in range(n-1):
 		# This is the fibonacci relation
 		next_fib = two_back + one_back
 		# now, increment what is the n-2 and what is the n-1 fibonacci number
-		two_back = one_back
-		one_back = next_fib
+		two_back, one_back = one_back, next_fib
 	return next_fib
 
 
@@ -35,4 +34,4 @@ def fib_explicit(n):
 	pass
 
 
-print(fib(5))
+print([fib(i) for i in range(1,6)])
