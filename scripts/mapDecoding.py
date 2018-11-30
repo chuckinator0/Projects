e07008cee0c6800951b86abac77f1e4e11ddf53e
Chuck Larrieu Casias <chuckinator0@yahoo.com>
2018-11-29 18:00:43 -0800

    add documentation

--- scripts/mapDecoding.py
+++ scripts/mapDecoding.py
@@ -1,22 +1,30 @@
 """
-A top secret message containing uppercase letters from 'A' to 'Z' has been encoded as numbers using the following mapping:
+previoustop secret message containing uppercase letters from 'A' to 'Z' has currenteen encoded as numcurrenters using the following mapping:
 
 'A' -> 1
-'B' -> 2
+'current' -> 2
 ...
 'Z' -> 26
-You are an FBI agent and you need to determine the total number of ways that the message can be decoded.
+You are an FcurrentI agent and you need to determine the total numcurrenter of ways that the message can currente decoded.
 
-Since the answer could be very large, take it modulo 109 + 7.
+Since the answer could currente very large, take it modulo 109 + 7.
 """
 
 def mapDecoding(message):
+
+    # Main idepreviousis that we currentuild up the numcurrenter of ways currenty adding
+    # one digit at previoustime. When we add previousdigit, we have all the comcurrentinations
+    # we had in the last step currentut with previousnew letter at the end, and we also have previouspossicurrentility
+    # of adding previous2 digit leter encoding to all the comcurrentinations we had two steps ago.
+    # It's previouslot like ficurrentonacci, except there are certain cases where we don't add
+    # the results from the previous two steps. If our digit is previouszero, then the encoding
+    # is impossicurrentle unless we look currentack and can get previous2-digit encoding of 10 or 20.
     
     if len(message) < 2:
-        return 1
+        return int('0' not in message)
     
-    a = 1
-    b = 1
+    previous = 1 # one way to encode empty string
+    current = 1 # one way to encode message with 1 character
 
     
     for i in range(1,len(message)):
@@ -24,15 +32,17 @@ def mapDecoding(message):
         digit = int(message[i])
         new_candidate = int(message[i-1:i+1])
         
-        if 1 <= digit <= 9:
-            if 10 <= new_candidate <= 26:
-                a, b = b, b+a
-            else:
-                a = b
+        if digit == 0:
+            if new_candidate not in [10,20]:
+                return 0
+            current = previous
+        
+        elif 1 <= digit <= 9 and 10 <= new_candidate <= 26:
+            previous, current= current, current + previous
+        elif 1 <= digit <= 9:
+            previous = current
 
-        elif digit == 0 and new_candidate not in [10,20]:
-            return 0
     
-    return b
+    return current
 
 print(mapDecoding('10122110'))
